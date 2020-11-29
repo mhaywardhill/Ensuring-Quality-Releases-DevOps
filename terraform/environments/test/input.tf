a68796e00ac02e8779b7de166b2918571cfc57fd
mat hayward-hill <haywardhill@gmail.com>
2020-11-29 13:09:55 +0000

    add all tf modules

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -1,3 +1,11 @@
 # Resource Group/Location
 variable "location" {}
 variable "resource_group" {}
+variable "application_type" {}
+
+# Network
+variable virtual_network_name {}
+variable address_prefix_test {}
+variable address_space {}
+
+variable vm_admin_username {}
