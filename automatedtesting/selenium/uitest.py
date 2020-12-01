744a667f3733c9972041f20cecad39f002bde338
mat hayward-hill <haywardhill@gmail.com>
2020-12-01 20:28:44 +0000

    add logging to unit test script

--- automatedtesting/selenium/uitest.py
+++ automatedtesting/selenium/uitest.py
@@ -1,46 +1,52 @@
-# #!/usr/bin/env python
+import logging 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 import time
-from datetime import datetime
 
-now = datetime.now().strftime("%m-%d-%y %H:%M:%S")
-print(now)
+timestamp = time.strftime('%b-%d-%Y_%H%M')
+output_file = "/var/log/selenium/uitest_"+timestamp+".log"
 
-url = "https://www.saucedemo.com/"
-inventory_url = "https://www.saucedemo.com/inventory.html"
+
+#Create and configure logger 
+logging.basicConfig(filename=output_file, 
+                    format='%(asctime)s %(message)s', 
+                    filemode='w') 
+  
+#Creating an object 
+logger=logging.getLogger() 
+  
+#Setting the threshold of logger to DEBUG 
+logger.setLevel(logging.DEBUG) 
 
 # Start the browser and login with standard_user
-def login (user, password):
-    print ("Logging in with" ,user, "with password", password)	
+def login (driver,user, password):
+    logger.info("Logging in with '%s' with password '%s'", user, password)	
     driver.find_element_by_id('user-name').send_keys(user)
     driver.find_element_by_id('password').send_keys(password)
     driver.find_element_by_id('login-button').click()
-    assert inventory_url in driver.current_url
-    print ('Test login success')
-	
+    logger.info ("Test login success")
+
 def add_to_cart(item_title, button):
-    print("Adding", item_title,  "to cart")
+    logger.info("Adding '%s' to cart",item_title)
     button.click()
 
 def remove_from_cart(item_title, button):
-    print("Removing",  item_title, "from cart")
+    logger.info("Removing '%s' from cart", item_title)
     button.click()
 
-if __name__ == "__main__":
-    print('#'*79)
-    print('Staring UI Test')
-    print('#'*79)
-    print ('Starting the browser...')
+def main():
+    logger.info("Starting the UI test")
+    logger.info("Starting the browser")
     options = ChromeOptions()
     options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
-    print ('Browser started successfully. Navigating to the demo page to login.')
-    driver.get(url)
-    
-    login('standard_user', 'secret_sauce')
+    logger.info("Browser started successfully. Navigating to the demo page to login")
+    driver.get("https://www.saucedemo.com/")
 
+    login(driver, 'standard_user', 'secret_sauce')
+
+    # Get all inventory items
     inventory_items = driver.find_elements(By.CLASS_NAME, "inventory_item")
 
     # Add all items
@@ -50,23 +56,25 @@ if __name__ == "__main__":
             By.CSS_SELECTOR, "button[class='btn_primary btn_inventory']"
         )
         add_to_cart(title, button)
-    
+
     # Go to basket
     cart_button = driver.find_element(
         By.CSS_SELECTOR, "a[class='shopping_cart_link fa-layers fa-fw']"
     )
     cart_button.click()
     
-    # Get items
+    # Get items in basket
     basket = driver.find_elements(By.CLASS_NAME, "cart_item")
-    
+
     # Remove items
     for item in basket:
         title = item.find_element(By.CLASS_NAME, "inventory_item_name").text
         button = item.find_element(
             By.CSS_SELECTOR, "button[class='btn_secondary cart_button']"
         )
-        remove_from_cart(title, button)
-    
-    print('#'*33, 'end of test', '#'*33)
-    
+        remove_from_cart(title, button)	
+
+    logger.info("Finished the UI test")
+
+if __name__ == "__main__":
+    main()
