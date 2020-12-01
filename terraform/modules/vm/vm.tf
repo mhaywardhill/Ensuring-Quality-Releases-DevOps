002ebc98a1d8a73480f86053c8f1105363741c60
mat hayward-hill <haywardhill@gmail.com>
2020-12-01 22:54:45 +0000

    remove deprecated formatting in tf modules

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,20 +1,20 @@
 resource "azurerm_network_interface" "main" {
   name                = "${var.application_type}-${var.resource_type}-nic"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+  location            = var.location
+  resource_group_name = var.resource_group
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = "${var.subnet_id}"
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${var.public_ip_address_id}"
+    public_ip_address_id          = var.public_ip_address_id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
   name                = "${var.application_type}-${var.resource_type}"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+  location            = var.location
+  resource_group_name = var.resource_group
   size                = "Standard_B1s"
   admin_username      = var.vm_admin_username
   network_interface_ids = [
