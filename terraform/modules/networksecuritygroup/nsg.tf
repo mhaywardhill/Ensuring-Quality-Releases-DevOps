db7a30d90dc8b226a43e782f301117094638adfc
mat hayward-hill <haywardhill@gmail.com>
2020-12-02 00:59:06 +0000

    updated network rules

--- terraform/modules/networksecuritygroup/nsg.tf
+++ terraform/modules/networksecuritygroup/nsg.tf
@@ -24,7 +24,18 @@ resource "azurerm_network_security_group" "nsg" {
         destination_port_range     = "22"
         source_address_prefix      = "*"
         destination_address_prefix = "*"
-    }
+  }
+  security_rule {
+    name                       = "loganalytics"
+    priority                   = 1001
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+   }
 }
 resource "azurerm_subnet_network_security_group_association" "test" {
     subnet_id                 = var.subnet_id
